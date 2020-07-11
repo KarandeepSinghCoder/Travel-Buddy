73410ab5b494d9e9cf9bb3900e733a777edeb980
KarandeepSinghCoder <karandchandi@gmail.com>
2020-07-11 17:51:38 +0530

    final

---
+++ modles/vitian.js
@@ -0,0 +1,10 @@
+var  mongoose= require("mongoose");
+var vitianSchema = new mongoose.Schema({
+    date: String,
+    trainnumber: String,
+    name: String,
+    number: String,
+    CarNumber: String,
+    CarSeat: String
+});
+module.exports = mongoose.model("vitian",vitianSchema);
